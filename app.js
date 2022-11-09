cf14c69017b947e7daa777ca8fbba5628b993d63
tufail-jatoi <tufailjatoi96@gmail.com>
2022-11-09 16:10:25 +0500

    anything

---
+++ app.js
@@ -0,0 +1,51 @@
+
+// ARRAY:-
+
+// instead of Using to many variables we use array for installing multiple values in 1 variable 
+
+// var a = ["orange","apple","grapes","mango","pomegranate"];
+
+
+// a[1] = "ABC";
+// a[6] = "apple";
+// a[8] = "apple";
+
+// a.push("Banana");
+// a.pop();
+
+// a.unshift("ABC");
+// a.shift();
+
+// // var b = a.slice(0,2);
+// a.splice(1,3);
+
+// var b = a.splice(3,1,"abc","xyz","mno");
+
+
+
+// console.log(a);
+// console.log(b);
+
+
+// LOOP
+// to do 1 thing multiple times 
+
+// var a = [];
+
+// for(var i=1;i<10;i++){
+//     a.push("ABC"+i);
+// }
+// console.log(a);
+
+
+// var a = ["abc","mnk","ikj","iuy","kjo","xyz"];
+
+// // console.log(a.length);
+
+// for(var i = 0; i < a.length; i++){
+//     alert(a[i]);
+
+
+// }
+
+
